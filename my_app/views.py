05e3ae3f7c463f6f3663151f31cb91a81ac38846
Rraghavendra123 <115609188+Rraghavendra123@users.noreply.github.com>
2023-05-15 14:41:22 +0530

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, World! and good morning gkufgyweouifgweoifgwefewffgwefwefwefwef')
+    return HttpResponse('Hello, World! and good morning gkufgyweouifgweoifgefwef')
 
